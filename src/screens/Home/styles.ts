dcc28e3dd50573eb109d79bfb60f55d4fda46ff5
Matheus Alves <mtalves27@gmail.com>
2022-11-03 14:27:38 -0300

    application completion

---
+++ src/screens/Home/styles.ts
@@ -0,0 +1,53 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#131016",
+    flex: 1,
+    padding: 24,
+  },
+  eventName: {
+    color: "#FDFCFE",
+    fontSize: 24,
+    fontWeight: "bold",
+    marginTop: 48,
+  },
+  eventDate: {
+    color: "#6B6B6B",
+    fontSize: 16,
+  },
+  input: {
+    flex: 1,
+    height: 56,
+    backgroundColor: "#1F1E25",
+    borderRadius: 5,
+    color: "#FDFCFE",
+    padding: 16,
+    fontSize: 16,
+    marginRight: 7,
+  },
+  button: {
+    width: 56,
+    height: 56,
+    borderRadius: 5,
+    backgroundColor: "#31CF67",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  buttonText: {
+    color: "#fff",
+    fontSize: 24,
+  },
+  form: {
+    width: "100%",
+    flexDirection: "row",
+    marginTop: 36,
+    marginBottom: 42,
+  },
+  listEmptyText: {
+    color: "#FFF",
+    fontSize: 14,
+    textAlign: "center",
+  },
+});
